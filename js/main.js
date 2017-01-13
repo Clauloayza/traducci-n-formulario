30b821e3f7e6095fa6c6be1d760f9e860ec1ec46
claudia <clou.7loymed@gmail.com>
2017-01-13 13:14:58 -0500

    cambio

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.getElementById("form-signin-heading").innerHTML="Por favor inicie sesión";
+
+document.getElementById("inputEmail").placeholder="Correo Electrónico";
+
+document.getElementById("inputPassword").placeholder="Contraseña";
+
+document.querySelector("span").innerHTML="Recordar datos";
+
+document.querySelector("button").innerHTML="Iniciar Sesión";
+
